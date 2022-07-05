1675935d2619b7457f999a8d79f14fb003543321
Ahmad Alsous <108018440+AhmadAlsous@users.noreply.github.com>
2022-07-05 20:36:29 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,45 @@
-if (performance.getEntriesByType("navigation")[0].type === "reload") {
-    rollDice();
-}
-function rollDice(){
-    var randomNumber1=Math.floor(Math.random()*6)+1;
-    var randomImg1="images/dice"+randomNumber1+".png";
-    document.querySelector(".img1").setAttribute("src",randomImg1);
-    var randomNumber2=Math.floor(Math.random()*6)+1;
-    var randomImg2="images/dice"+randomNumber2+".png";
-    document.querySelector(".img2").setAttribute("src",randomImg2);
-    if(randomNumber1>randomNumber2){
-    document.querySelector("h1").innerHTML="🚩Player 1 Wins!";
-    }else if(randomNumber1<randomNumber2){
-        document.querySelector("h1").innerHTML="Player 2 Wins!🚩";
-    }else{
-        document.querySelector("h1").innerHTML="Draw!";
+function call() {
+  let player1 = document.querySelector("#player1").value;
+  let player2 = document.querySelector("#player2").value;
+  let speed = 600 - document.querySelector("#speed").value;
+  document.querySelector("h1").innerHTML = "Dice Game";
+  let i = 0;
+  const diceID = setInterval(function () {
+    if (i === 10) {
+      clearInterval(diceID);
+    } else {
+      i++;
+      let s = randomDice(player1, player2);
+      if (i === 10) {
+        document.querySelector(".img1").style.transform = "rotate(0deg)";
+        document.querySelector(".img2").style.transform = "rotate(0deg)";
+        document.querySelector("h1").innerHTML = s;
+      }
     }
+  }, speed);
 }
+
+function randomDice(name1, name2) {
+  var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+  var randomImg1 = "images/dice" + randomNumber1 + ".png";
+  let randomDeg1 = Math.floor(Math.random() * 91);
+  document.querySelector(".img1").setAttribute("src", randomImg1);
+  document.querySelector(".img1").style.transform =
+    "rotate(" + randomDeg1 + "deg)";
+  var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+  var randomImg2 = "images/dice" + randomNumber2 + ".png";
+  let randomDeg2 = Math.floor(Math.random() * 91);
+  document.querySelector(".img2").setAttribute("src", randomImg2);
+  document.querySelector(".img2").style.transform =
+    "rotate(" + randomDeg2 + "deg)";
+  if (randomNumber1 > randomNumber2) {
+    return "🚩 " + name1 + " Wins!";
+  } else if (randomNumber1 < randomNumber2) {
+    return name2 + " Wins!🚩";
+  } else {
+    return "Draw!";
+  }
+}
+
+document.querySelector(".btn").addEventListener("click", call);
+
